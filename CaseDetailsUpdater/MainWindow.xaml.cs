ea5f94315327bd2ad86674bd96208a556d7bd8eb
Ikenna Oparaugo <ikenna.oparaugo@pwc.com>
2022-09-17 15:02:47 +0400

    add case updater code

---
+++ CaseDetailsUpdater/MainWindow.xaml.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System.Configuration;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk.Messages;
+using System.Net;
+
+namespace CaseDetailsUpdater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CrmServiceClient CrmServiceClient = null;
+        bool connectionState = false;
+        Resources.CrmQuery crmQuery = new Resources.CrmQuery();
+        string fetchXML;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+         
+        }
+
+        private void connectToCRM()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["Xrm"].ConnectionString;
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                    CrmServiceClient = new CrmServiceClient(connectionString);
+                    if (!CrmServiceClient.IsReady)
+                    {
+                        connectstatuslbl.Content = "Unable to connect to CRM";
+                        connectstatuslbl.Background = Brushes.Red;
+                        connectstatuslbl.Foreground = Brushes.White;
+                    }
+                    else
+                    {
+                        connectionState = true;
+                        connectstatuslbl.Content = $"Connected to {CrmServiceClient.ConnectedOrgFriendlyName}";
+                        connectstatuslbl.Background = Brushes.Green;
+                        connectstatuslbl.Foreground = Brushes.White;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                connectstatuslbl.Content = ex.Message;
+            }
+        }
+
+        private void crmConnectbtn_Click(object sender, RoutedEventArgs e)
+        {
+            crmConnectbtn.IsEnabled = false;
+            if (!connectionState)
+            {
+                connectToCRM();
+                
+            }
+            else
+            {
+                crmConnectbtn.IsEnabled = true;
+            }
+        }
+
+        private void executeqrybtn_Click(object sender, RoutedEventArgs e)
+        {
+            statuslbl.Content = "Retrieving record count.....";
+            executeqrybtn.IsEnabled = false;
+             var pageNumber = 1;
+            var pagingCookie = string.Empty;
+            var result = new List<Entity>();
+            EntityCollection entCasesToUpdate;
+            //read the fetch xml
+            fetchXML = fetchqry.Text;
+            FetchXmlToQueryExpressionRequest queryExpressionRequest = new FetchXmlToQueryExpressionRequest()
+            {
+                FetchXml = fetchXML
+            };
+
+           FetchXmlToQueryExpressionResponse resQuery =  (FetchXmlToQueryExpressionResponse)CrmServiceClient.Execute(queryExpressionRequest);
+           QueryExpression queryExpression = resQuery.Query;
+            if (!string.IsNullOrEmpty(fetchXML))
+            {
+                
+                do
+                {
+                    if (pageNumber != 1)
+                    {
+                        queryExpression.PageInfo.PageNumber = pageNumber;
+                        queryExpression.PageInfo.PagingCookie = pagingCookie;
+                    }
+
+                    entCasesToUpdate = CrmServiceClient.RetrieveMultiple(queryExpression);
+                    //Exeute query and fetch the results
+
+                    if (entCasesToUpdate != null && entCasesToUpdate.Entities.Count > 0)
+                    {
+                        if (entCasesToUpdate.MoreRecords)
+                        {
+                            pageNumber++;
+                            pagingCookie = entCasesToUpdate.PagingCookie;
+                        }
+                        result.AddRange(entCasesToUpdate.Entities);
+                        
+                    }
+                    
+                }
+                while (entCasesToUpdate.MoreRecords);
+                statuslbl.Content = $"Total number of Records is {result.Count}";
+                executeqrybtn.IsEnabled = true;
+                //update case records 
+               ExecuteUpdateCase(result);
+            }
+        }
+
+        private void ExecuteUpdateCase(List<Entity> entities)
+        {
+          
+            //get the case record and check if the case status is resolved
+            statuslbl.Content = "Updating cases....";
+                foreach (Entity updatecase in entities)
+                {
+                int intialStateCode = updatecase.GetAttributeValue<OptionSetValue>("statecode").Value;
+                int intialStatusCode = updatecase.GetAttributeValue<OptionSetValue>("statuscode").Value;
+                if (intialStateCode != 0) // case is resolved or cancelled
+                {
+                    //reactivate the case
+                    SetStateRequest setStateRequest = new SetStateRequest()
+                    {
+                        // Set the Request Object's Properties
+                        State = new OptionSetValue(0), // Active
+                        Status = new OptionSetValue(1), // Inprogress
+
+
+                        // Point the Request to the case whose state is being changed
+                        EntityMoniker = updatecase.ToEntityReference()
+                    };
+
+                     CrmServiceClient.Execute(setStateRequest);
+
+                    //Update the case details to International City
+                    string[] cols = { "tc_city" };
+                    Entity caseToUpdate = CrmServiceClient.Retrieve("incident", updatecase.Id, new ColumnSet(cols)) ;
+                    caseToUpdate.Attributes["tc_city"] = new EntityReference("cc_city", Guid.Parse(cityGuid.Text));
+                    CrmServiceClient.Update(caseToUpdate);
+
+                    // Close case back to original state
+
+                    //reactivate the case
+                    var resolution = new Entity("incidentresolution");
+                    resolution["subject"] = "Close Case";
+                    resolution["incidentid"] = new EntityReference { Id = caseToUpdate.Id, LogicalName = "incident" };
+                    var closeIncidentRequest = new CloseIncidentRequest
+                    {
+                        IncidentResolution = resolution,
+                        Status = new OptionSetValue(intialStatusCode),
+                        
+                    };
+                    CrmServiceClient.Execute(closeIncidentRequest);
+                }
+
+                else
+                {
+                    //Update the case details to International City
+                    updatecase.Attributes["tc_city"] = new EntityReference("cc_city", Guid.Parse(cityGuid.Text));
+                    CrmServiceClient.Update(updatecase);
+                }
+                }
+
+            statuslbl.Content = "Case Update Completed...";
+
+
+
+
+
+        }
+
+        private void qryselectorbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            executeqrybtn.IsEnabled = false;
+            cityGuid.Text = string.Empty;
+            int selectedItem = qryselectorbox.SelectedIndex;
+            if (selectedItem == 0) //applicable to international cities
+            {
+                fetchqry.Text = crmQuery.GetQueryForApplicableInternationalCities();
+                cityGuid.Text = "4CB11407-FBDF-EB11-B808-0050560B5C4A"; //International
+                
+            }
+
+            else if (selectedItem == 1) //not applicable to international cities
+            {
+                fetchqry.Text = crmQuery.GetQueryForNotApplicableInternationalCities();
+                cityGuid.Text = "6A4EDFC3-8343-EB11-BB23-000D3A22C1F6";//riyard
+            }
+
+            else if (selectedItem == 2) //Test
+            {
+                fetchqry.Text = crmQuery.GetDevResolvedCases();
+                cityGuid.Text = "2D986ACB-4270-EB11-B804-0050560B5C49";//riyard
+            }
+
+            if (!string.IsNullOrEmpty(fetchqry.Text))
+            {
+                executeqrybtn.IsEnabled = true;
+            }
+        }
+
+        private void qryselectorbox_Initialized(object sender, EventArgs e)
+        {
+            int selectedItem = qryselectorbox.SelectedIndex;
+            if (selectedItem == 0) //applicable to international cities
+            {
+                fetchqry.Text = crmQuery.GetQueryForApplicableInternationalCities();
+            }
+
+            else if (selectedItem == 1)
+            {
+                fetchqry.Text = crmQuery.GetQueryForNotApplicableInternationalCities();
+            }
+        }
+
+        private void GetEmailTemplate()
+        {
+            Entity[] entAryFromPartyList = null, entAryToPartyList = null;
+            EntityReference entrefRegardingObject = new EntityReference("incident", new Guid("7656623E-C9A8-EB11-B806-0050560B5C48"));
+            InstantiateTemplateRequest rqtInstantiateTemplate = new InstantiateTemplateRequest
+            {
+                TemplateId = Guid.Parse("04E18245-5C33-ED11-B815-0050560B5C48"),//emailObjInputParams.entTemplate.Id,
+                ObjectId = Guid.Parse("7656623E-C9A8-EB11-B806-0050560B5C48"),
+                ObjectType = "incident"
+             };
+
+                InstantiateTemplateResponse rspInstantiateTemplate = (InstantiateTemplateResponse)CrmServiceClient.Execute(rqtInstantiateTemplate);
+
+                if (rspInstantiateTemplate != null && rspInstantiateTemplate.EntityCollection != null && rspInstantiateTemplate.EntityCollection.Entities != null
+                    && rspInstantiateTemplate.EntityCollection.Entities.Count > 0)
+                {
+                string newBody;
+                Entity entEmailTemplate = rspInstantiateTemplate.EntityCollection.Entities.FirstOrDefault();
+                   string strEmailSubject = entEmailTemplate.GetAttributeValue<string>("subject");
+                   string strEmailBody = entEmailTemplate.GetAttributeValue<string>("description");
+
+                newBody = strEmailBody.Contains("Case:Record GUID")
+                    ? strEmailBody.Replace("Case:Record%20GUID", "7656623E-C9A8-EB11-B806-0050560B5C48")
+                    : strEmailBody;
+
+                Entity entFromAP = new Entity("activityparty");
+                entFromAP.Attributes["partyid"] = new EntityReference("systemuser", Guid.Parse("3FB054CD-E66A-EB11-B804-0050560B5C48"));
+                entAryFromPartyList = new Entity[] { entFromAP };
+
+                Entity entToAP = new Entity("activityparty");
+                entToAP.Attributes["partyid"] =new EntityReference("systemuser", Guid.Parse("3FB054CD-E66A-EB11-B804-0050560B5C48"));
+                entAryToPartyList = new Entity[] { entToAP };
+
+
+                Entity entCreateEmail = new Entity("email");
+
+                if (entAryFromPartyList != null && entAryFromPartyList.Length > 0)
+                {
+                    entCreateEmail.Attributes["from"] = entAryFromPartyList;
+                }
+               
+                if (entAryToPartyList != null && entAryToPartyList.Length > 0)
+                {
+                    entCreateEmail.Attributes["to"] = entAryToPartyList;
+                }
+                
+                if (!string.IsNullOrWhiteSpace(strEmailSubject))
+                {
+                    entCreateEmail.Attributes["subject"] = strEmailSubject;
+                }
+                
+                if (!string.IsNullOrWhiteSpace(newBody))
+                {
+                    entCreateEmail.Attributes["description"] = newBody;
+                }
+                
+                if (entrefRegardingObject != null)
+                {
+                    entCreateEmail.Attributes["regardingobjectid"] = entrefRegardingObject;
+                }
+
+                entCreateEmail.Id = CrmServiceClient.Create(entCreateEmail);
+
+                if (entCreateEmail.Id != Guid.Empty)
+                {
+                    SendEmailRequest rqtSendEmail = new SendEmailRequest
+                    {
+                        IssueSend = true,
+                        EmailId = entCreateEmail.Id
+                    };
+
+                    SendEmailResponse rspSendEmail = (SendEmailResponse)CrmServiceClient.Execute(rqtSendEmail);
+                }
+
+            }
+            
+        }
+
+        
+    }
+}
---
+++ CaseDetailsUpdater/Resources/CrmQuery.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseDetailsUpdater.Resources
+{
+    public class CrmQuery
+    {
+        internal string GetQueryForApplicableInternationalCities()
+        {
+            string strXML = string.Empty;
+
+            strXML = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+  <entity name='incident'>
+    <attribute name='createdon' />
+    <attribute name='incidentid' />
+    <attribute name='customerid' />
+    <attribute name='ownerid' />
+    <attribute name='statuscode' />
+    <attribute name='tc_subcategory' />
+    <attribute name='tc_category' />
+    <attribute name='tc_casetype' />
+    <attribute name='firstresponseslastatus' />
+    <attribute name='tc_caserefno' />
+    <order attribute='createdon' descending='true' />
+    <filter type='and'>
+      <condition attribute='tc_city' operator='in'>
+        <value uiname='All Cities' uitype='cc_city'>{6586E025-3DD3-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International departure' uitype='cc_city'>{4EC788CE-D78C-EC11-B807-0050560B5C4B}</value>
+      </condition>
+      <condition attribute='tc_subcategory' operator='in'>
+        <value uiname='Issue with VisitSaudi.com/App' uitype='tc_casesubcategory'>{14816C84-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International Provider - Fake offer' uitype='tc_casesubcategory'>{2F77B080-7EDD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International Provider - Contract terms between the visitor and the provider' uitype='tc_casesubcategory'>{E63FD6D1-82DD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International Provider - Refund Issue' uitype='tc_casesubcategory'>{E86CC5F1-82DD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International Provider -  Cancellation of reservation without request' uitype='tc_casesubcategory'>{3DA2DC06-83DD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='MT website' uitype='tc_casesubcategory'>{6F62958A-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='MT platform' uitype='tc_casesubcategory'>{7162958A-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Booking an experience via VisitSaudi.com' uitype='tc_casesubcategory'>{9D87169D-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='E-Visa: Technical Issue with Health Insurance' uitype='tc_casesubcategory'>{367C69A9-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='E-Visa: Technical Issue with Application' uitype='tc_casesubcategory'>{387C69A9-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='E-Visa: Payment Issue' uitype='tc_casesubcategory'>{3A7C69A9-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Visa Cancellation Issue' uitype='tc_casesubcategory'>{406DA71C-0A93-EC11-B807-0050560B5C4B}</value>
+        <value uiname='License application' uitype='tc_casesubcategory'>{8587169D-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Renewal of license' uitype='tc_casesubcategory'>{9187169D-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Incorrect payment processed by MT' uitype='tc_casesubcategory'>{C5CB52A3-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Issue with MT employee' uitype='tc_casesubcategory'>{E0928D92-3ABA-EC11-B80A-0050560B5C4A}</value>
+        <value uiname='No response to raised complaint' uitype='tc_casesubcategory'>{318098AD-3ABA-EC11-B80A-0050560B5C4A}</value>
+        <value uiname='Issue with fine from MT' uitype='tc_casesubcategory'>{9787169D-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Visa on Arrival Issue' uitype='tc_casesubcategory'>{18816C84-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Issue with boarding the plane to KSA' uitype='tc_casesubcategory'>{9F87169D-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Entry Point Issue' uitype='tc_casesubcategory'>{3C7C69A9-AFD1-EB11-B808-0050560B5C4A}</value>
+      </condition>
+    </filter>
+    <link-entity name='contact' from='contactid' to='customerid' visible='false' link-type='outer' alias='a_f5be541c782ceb11a813000d3a6540e8'>
+      <attribute name='tc_preferredcommunicationlanguage' />
+    </link-entity>
+  </entity>
+</fetch>";
+
+            return strXML;
+        }
+
+        internal string GetQueryForNotApplicableInternationalCities()
+        {
+            string strXML = string.Empty;
+
+            strXML = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+  <entity name='incident'>
+    <attribute name='createdon' />
+    <attribute name='incidentid' />
+    <attribute name='customerid' />
+    <attribute name='ownerid' />
+    <attribute name='statuscode' />
+    <attribute name='tc_subcategory' />
+    <attribute name='tc_category' />
+    <attribute name='tc_casetype' />
+    <attribute name='firstresponseslastatus' />
+    <attribute name='tc_caserefno' />
+    <attribute name='tc_city' />
+    <order attribute='createdon' descending='true' />
+    <filter type='and'>
+      <condition attribute='tc_city' operator='in'>
+        <value uiname='All Cities' uitype='cc_city'>{6586E025-3DD3-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International departure' uitype='cc_city'>{4EC788CE-D78C-EC11-B807-0050560B5C4B}</value>
+      </condition>
+      <condition attribute='tc_subcategory' operator='in'>
+        <value uiname='VisitSaudi Kiosk/Dispenser' uitype='tc_casesubcategory'>{8B87169D-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Domestic Provider - Poor service' uitype='tc_casesubcategory'>{1E799BAF-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Domestic Provider - Fake offer' uitype='tc_casesubcategory'>{20799BAF-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Domestic Provider - Contract terms between the visitor and the provider' uitype='tc_casesubcategory'>{22799BAF-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Domestic Provider - Cancellation of reservation without request' uitype='tc_casesubcategory'>{24799BAF-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Domestic Provider - Refund Issue' uitype='tc_casesubcategory'>{26799BAF-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Domestic Provider - Non-adherence to COVID Guidelines' uitype='tc_casesubcategory'>{28799BAF-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Domestic Provider -  Mistreatment / refusal of entry' uitype='tc_casesubcategory'>{1DE1F7B5-AFD1-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Domestic Provider - No booking upon arrival at accommodation' uitype='tc_casesubcategory'>{31C27D10-7EDD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International Provider - Poor service' uitype='tc_casesubcategory'>{33CD8240-7EDD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International Provider - Non-adherence to COVID Guidelines' uitype='tc_casesubcategory'>{4FFB2E1C-83DD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International Provider - Mistreatment / refusal of entry' uitype='tc_casesubcategory'>{7818274D-83DD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='International Provider - No booking upon arrival at accommodation' uitype='tc_casesubcategory'>{0F975063-83DD-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Poor facilities or health &amp; Safety concerns' uitype='tc_casesubcategory'>{6C06CA84-F9DE-EB11-B808-0050560B5C4A}</value>
+        <value uiname='Changing Tawakalna Status' uitype='tc_casesubcategory'>{58648A09-A381-EC11-B80A-0050560B5C4A}</value>
+      </condition>
+    </filter>
+    <link-entity name='contact' from='contactid' to='customerid' visible='false' link-type='outer' alias='a_f5be541c782ceb11a813000d3a6540e8'>
+      <attribute name='tc_preferredcommunicationlanguage' />
+    </link-entity>
+  </entity>
+</fetch>";
+
+            return strXML;
+        }
+
+
+      internal string GetDevResolvedCases()
+        {
+            string strXml = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+  <entity name='incident'>
+    <attribute name='createdon' />
+    <attribute name='customerid' />
+    <attribute name='ownerid' />
+    <attribute name='statuscode' />
+    <attribute name='statecode' />
+    <attribute name='tc_subcategory' />
+    <attribute name='tc_category' />
+    <attribute name='tc_casetype' />
+    <attribute name='incidentid' />
+    <attribute name='firstresponseslastatus' />
+    <attribute name='tc_caserefno' />
+    <attribute name='tc_city' />
+    <order attribute='createdon' descending='true' />
+    <filter type='and'>
+      <condition attribute='statecode' operator='eq' value='1' />
+    </filter>
+    <link-entity name='contact' from='contactid' to='customerid' visible='false' link-type='outer' alias='a_f5be541c782ceb11a813000d3a6540e8'>
+      <attribute name='tc_preferredcommunicationlanguage' />
+    </link-entity>
+  </entity>
+</fetch>";
+            return strXml;
+        }
+    }
+}
